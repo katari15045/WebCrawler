c42da0c27c139776ed4a980ed53e202c7350665a
katari Saketh Ram <katari15045@iiitd.ac.in>
2017-03-09 19:38:32 +0530

    Working 1.GetAllLinksInAPage Able to run it from terminal

---
+++ code/1.GetAllLinksInAPage/Sample.java
@@ -0,0 +1,49 @@
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+public class Sample 
+{
+	private static Document document;
+	private static org.jsoup.select.Elements links;
+	private static String targetURL;
+	
+	public static void main(String[] args)
+	{
+		extractDocument();
+		extractLinks();
+		printLinks();
+	}
+	
+	private static void extractDocument()
+	{
+		document = null;
+		
+		try
+		{
+			targetURL = "http://explorelinux.github.io/index.html";
+			document = Jsoup.connect(targetURL).get();
+		}
+		
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	private static void extractLinks()
+	{
+		links = document.select("a");
+	}
+	
+	private static void printLinks()
+	{
+		for(Element e:links )
+		{
+			System.out.println( e.attr("abs:href") );
+		}
+	}
+}
+
