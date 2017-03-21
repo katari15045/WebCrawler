b559b68076329c7cea9451c47e1d069ef1cc271e
katari Saketh Ram <katari15045@iiitd.ac.in>
2017-03-21 18:56:19 +0530

    Working Decreased no.of importing packages

--- code/2.HttpClient/HttpClient.java
+++ code/2.HttpClient/HttpClient.java
@@ -1,5 +1,11 @@
-import java.io.*;
-import java.net.*;
+import java.lang.Exception;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.ProtocolException;
+import java.net.MalformedURLException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class HttpClient
 {
