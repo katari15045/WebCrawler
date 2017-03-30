0d4d050ca9f20b5dc4d43ace09dc479936bc08b2
katari Saketh Ram <katari15045@iiitd.ac.in>
2017-03-30 22:41:23 +0530

    Working Able to access Nutch from Java Code -> Shell scripting is used It took 30 seconds for nutch to take links from seed.txt and display the results of the crawl.

---
+++ code/5.AccessNutchFromJavaCode/sample.sh
@@ -0,0 +1,19 @@
+
+
+cd /opt/apache-nutch-1.12/
+sudo rm -rf crawl/crawldb
+sudo rm -rf crawl/segments
+sudo mkdir crawl/crawldb
+sudo chmod 777 crawl/crawldb
+sudo mkdir crawl/segments
+sudo chmod 777 crawl/segments
+bin/nutch inject crawl/crawldb urls/
+bin/nutch generate crawl/crawldb crawl/segments -topN 10
+bin/nutch fetch crawl/segments/20*
+bin/nutch parse crawl/segments/20*
+bin/nutch updatedb crawl/crawldb crawl/segments/20*
+bin/nutch invertlinks crawl/linkdb -dir crawl/segments
+rm -rf output
+mkdir output
+bin/nutch readseg -dump crawl/segments/20*/ output -nocontent -nofetch - nogenerate -noparse -noparsetext
+cat output/dump | grep outlink
