f24cdadf1fb8ddd6262d0c914419ff1b40727b3d
Saketh Katari <katari15045@iiitd.ac.in>
2017-05-15 12:03:57 +0530

    Woriking Able to take query string and that can be of multiple words

--- code/3.HttpClient/sample.java
+++ code/3.HttpClient/sample.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 
 public class sample
 {
@@ -8,11 +8,45 @@ public class sample
 
 	public static void main(String[] args) throws Exception
 	{
-		url = "https://www.gigablast.com/search?q=test&format=xml&userid=12345&code=demo";
-		httpClient = new HttpClient(url);
-		httpClient.sendGetRequest();
-		result = httpClient.getResult();
+		String userStr = takeUserInput();
+		String queryString = prepareQueryString(userStr);
+		System.out.println(queryString);
+		//url = "https://www.gigablast.com/search?q=test&format=xml&userid=12345&code=demo";
+		//httpClient = new HttpClient(url);
+		//httpClient.sendGetRequest();
+		//result = httpClient.getResult();
 
-		System.out.println(result);
+		//System.out.println(result);
+	}
+
+	private static String takeUserInput()
+	{
+		Scanner scanner = new Scanner(System.in);
+		String inpStr;
+		System.out.print("Search for -> ");
+		inpStr = scanner.nextLine();
+
+		return inpStr;
+	}
+
+	private static String prepareQueryString(String inpStr)
+	{
+		StringBuilder result = new StringBuilder();
+		int count = 0;
+
+		for(String str:inpStr.split(" "))
+		{
+			if( count != 0 )
+			{
+				result.append("\\ ");
+			}
+
+			//System.out.println(str);
+			result.append(str);
+
+			count = count + 1;
+		}
+
+		return result.toString();
 	}
 }
